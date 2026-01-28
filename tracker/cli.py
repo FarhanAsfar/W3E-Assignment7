c65ef5da8fed3ade4d61bd230d56d12cb18ece70
farhan <farhanasfar21@gmail.com>
2026-01-28 17:19:28 +0600

    Parse 'list' command to show data from file

--- tracker/cli.py
+++ tracker/cli.py
@@ -2,10 +2,13 @@ import argparse
 from .storage import create_expense
 
 def parse_argument():
-    parser = argparse.ArgumentParser(description="")
+    parser = argparse.ArgumentParser(description="Expense Tracker CLI")
     subparser = parser.add_subparsers(dest="command")
 
     add_parser = subparser.add_parser("add")
+    list_parser = subparser.add_parser("list")
+
+
     add_parser.add_argument("--id", required=True)
     add_parser.add_argument("--date", required=True)
     add_parser.add_argument("--category", required=True)
@@ -13,7 +16,7 @@ def parse_argument():
     add_parser.add_argument("--currency", required=True)
     add_parser.add_argument("--note", default="")
 
-    args = parser.parse_args();
+    args = parser.parse_args()
 
     if args.command == "add":
         new_expense = {
@@ -25,6 +28,10 @@ def parse_argument():
             "note": args.note,
         }
         create_expense(new_expense)
+    elif args.command == "list":
+        from .storage import list_expenses
+        list_expenses()
+    
 
 if __name__ == "__main__":
     parse_argument()
