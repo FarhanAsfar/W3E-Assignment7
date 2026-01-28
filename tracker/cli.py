6aa17d2a6d7cb49740becbaa7e155151e8079313
farhan <farhanasfar21@gmail.com>
2026-01-28 15:44:45 +0600

    Parse argument for 'add' command

---
+++ tracker/cli.py
@@ -0,0 +1,30 @@
+import argparse
+from .storage import create_expense
+
+def parse_argument():
+    parser = argparse.ArgumentParser(description="")
+    subparser = parser.add_subparsers(dest="command")
+
+    add_parser = subparser.add_parser("add")
+    add_parser.add_argument("--id", required=True)
+    add_parser.add_argument("--date", required=True)
+    add_parser.add_argument("--category", required=True)
+    add_parser.add_argument("--amount", type=float, required=True)
+    add_parser.add_argument("--currency", required=True)
+    add_parser.add_argument("--note", default="")
+
+    args = parser.parse_args();
+
+    if args.command == "add":
+        new_expense = {
+            "id": args.id,
+            "date": args.date,
+            "category": args.category,
+            "amount": args.amount,
+            "currency": args.currency,
+            "note": args.note,
+        }
+        create_expense(new_expense)
+
+if __name__ == "__main__":
+    parse_argument()
