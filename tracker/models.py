1be37fde0f0cfe2aedd3da0bef6e221c8093f6ec
farhan <farhanasfar21@gmail.com>
2026-01-28 12:38:26 +0600

    Add a 'to_dict()' method
    This method is needed to return a dictionary representation of a instance, it avoids the TypeError:..not JSON serializable

--- tracker/models.py
+++ tracker/models.py
@@ -9,5 +9,15 @@ class Expense(BaseModel):
     currency: str = "BDT"
     note: str
     created_at: datetime = Field(default_factory=datetime.now)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "date": self.date,
+            "category": self.category,
+            "amount": self.amount,
+            "currency": self.currency,
+            "note": self.note,
+        }
          
 
