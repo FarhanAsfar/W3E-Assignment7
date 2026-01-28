f56f705b99112d56ee40d8c8925b6119a6466ecc
farhan <farhanasfar21@gmail.com>
2026-01-28 18:47:52 +0600

    Generate id in a specific format and increment it's suffix

--- tracker/storage.py
+++ tracker/storage.py
@@ -1,31 +1,43 @@
 from .models import Expense
+from datetime import datetime
 import os
 import json
+
+
+date_str = datetime.now().strftime("%Y%m%d")
+    
 
 def create_expense(expense_data):
     file_path = "./data/expenses.json"
 
     # making sure if path exists
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    
-    # expense1 = Expense (
-    #     id = "001",
-    #     date = "2026-01-28",
-    #     category = "food",
-    #     amount = 333.33,
-    #     note = "lunch"
-    # )
-
-    expense_dataa = Expense(**expense_data)
 
     try:
         if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
             with open (file_path, 'r') as json_file:
                 data = json.load(json_file)
+                #if only one data exist, then wrap it inside []
+                if isinstance(data, dict):
+                    data = [data]
         else:
             data = [{"version": "1"},]
         
-        data.append(expense_dataa.model_dump(mode='json'))
+        if data:
+            last_id = data[-1].get('id', 'EXP-202060128-0000')
+            try:
+                last_seq = int(last_id.split('-')[-1])
+            except (ValueError, IndexError):
+                last_seq = 0
+        else:
+            last_seq = 0
+        
+        new_serial = last_seq + 1
+        new_id = f"EXP-{date_str}-{new_serial:04d}"
+        expense_data['id'] = new_id
+        expense_obj = Expense(**expense_data)
+
+        data.append(expense_obj.model_dump(mode='json'))
 
         with open (file_path, 'w') as json_file:
             json.dump(data, json_file, indent=4)
